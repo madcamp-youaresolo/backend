c45cc5ece7f0d03c0a90d44a9a9d7d40ea480f8a
hayeyoung <hayeyoung4031@naver.com>
2025-07-21 23:14:23 +0900

    fix: 통계 조회 수정

--- src/index.js
+++ src/index.js
@@ -13,13 +13,29 @@ app.post('/api/profiles', async (req, res) => {
     return res.status(400).json({ error: '모든 필드를 입력해주세요.' });
   }
   try {
-    const { rows } = await db.query(
-      `INSERT INTO profiles (nickname, gender, result_type)
-       VALUES ($1,$2,$3)
-       RETURNING id, nickname, gender, result_type, created_at`,
-      [nickname, gender, resultType]
+    const { rows:[profile] } = await db.query(
+      `INSERT INTO profiles (nickname, gender)
+       VALUES ($1,$2) RETURNING id`, [nickname, gender]
     );
-    res.status(201).json(rows[0]);
+
+    for (let name of resultType) {
+      const { rows:[t] } = await db.query(
+        `INSERT INTO types (name) VALUES ($1)
+        ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name
+        RETURNING id`, [name]
+    );
+    const typeId = t ? t.id : (
+        await db.query(`SELECT id FROM types WHERE name = $1`, [name])
+      ).rows[0].id;
+    await db.query(
+      `INSERT INTO profile_types (profile_id, type_id)
+       VALUES ($1, $2)`, [profile.id, t.id]
+      );
+  }
+  return res.status(201).json({
+      message: '프로필 저장완료',
+      profileId: profile.id
+    });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: '서버 오류' });
@@ -30,16 +46,17 @@ app.post('/api/profiles', async (req, res) => {
 app.get('/api/stats', async (req, res) => {
   try {
     const { rows: typeCounts } = await db.query(`
-      SELECT result_type, COUNT(*) AS count
-      FROM profiles
-      GROUP BY result_type
+      SELECT
+        t.name   AS result_type,
+        COUNT(*) AS count
+      FROM profile_types pt
+      JOIN types t
+        ON pt.type_id = t.id
+      GROUP BY t.name
+      ORDER BY count DESC
     `);
-    const { rows: genderType } = await db.query(`
-      SELECT gender, result_type, COUNT(*) AS count
-      FROM profiles
-      GROUP BY gender, result_type
-    `);
-    res.json({ typeCounts, genderType });
+
+    return res.json({ typeCounts });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: '서버 오류' });
