a06c80b7f18141954fe4772457f9c7d88225c254
Yeonjae Lee <lyeonj@sookmyung.ac.kr>
2025-07-23 03:51:22 +0900

    Update db.js

--- src/db.js
+++ src/db.js
@@ -3,8 +3,11 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  // ssl: { rejectUnauthorized: false }  // 필요 시 SSL 옵션
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
 });
 
 module.exports = {
